31e2dc81123474890a50fbd01bf558d10f9394c3
Peter <pavlatos.peter@gmail.com>
2022-06-10 09:07:26 -0400

    Removed keys to api for security

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Key': 'ebf597c0d9msh75d9a56ef456712p19ccf8jsn677e0f798bae',
-    'X-RapidAPI-Host': 'crypto-ranking-data.p.rapidapi.com'
+    'X-RapidAPI-Key': '',
+    'X-RapidAPI-Host': ''
 }
 const baseUrl = 'https://crypto-ranking-data.p.rapidapi.com/market/list/1/100';
 
